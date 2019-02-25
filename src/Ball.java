fecdde44a93332582ac184e22fc1cf4f3746a1ac
ars1004 <ars1004@alu.ubu.es>
2019-02-25 09:56:40 +0100

    Cambiado el tamaño de la bola para que sea igual al de la imagen.

--- src/Ball.java
+++ src/Ball.java
@@ -13,10 +13,8 @@ public class Ball {
 		ImageIcon ii = new ImageIcon(this.getClass().getResource(Ball));
 		image = ii.getImage();
 		
-		//TODO Depend of image size
-		IMG_TAM_X = 32;
-		IMG_TAM_Y = 32;
-
+		IMG_TAM_X = image.getWidth(null);
+		IMG_TAM_Y = image.getHeight(null);
 		
 		x = Billiards.Width/4-16;
 		y = Billiards.Height/2-16;
