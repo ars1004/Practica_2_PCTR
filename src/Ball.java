6d31d4ec0f309bf6682bce30530564af52ff4fe4
ars1004 <ars1004@alu.ubu.es>
2019-02-25 10:03:39 +0100

    Añadidas postcondiciones.

--- src/Ball.java
+++ src/Ball.java
@@ -1,72 +1,75 @@
 import java.awt.Image;
 import javax.swing.ImageIcon;
+
 //TODO Transform the code to be used safely in a concurrent context.  
 public class Ball {
-	private String Ball = "Ball.png"; 
+	private String Ball = "Ball.png";
 
-	private double x,y,dx,dy;
-	private double v,fi;
+	private double x, y, dx, dy;
+	private double v, fi;
 	private Image image;
-	private final int IMG_TAM_X,IMG_TAM_Y;
+	private final int IMG_TAM_X, IMG_TAM_Y;
 
 	public Ball() {
 		ImageIcon ii = new ImageIcon(this.getClass().getResource(Ball));
 		image = ii.getImage();
-		
+
 		IMG_TAM_X = image.getWidth(null);
 		IMG_TAM_Y = image.getHeight(null);
-		
-		x = Billiards.Width/4-16;
-		y = Billiards.Height/2-16;
+
+		x = Billiards.Width / 4 - 16;
+		y = Billiards.Height / 2 - 16;
 		v = 5;
-		fi =  Math.random() * Math.PI * 2;
+		fi = Math.random() * Math.PI * 2;
 	}
 
 	public void move() {
-		v = v*Math.exp(-v/1000);
-		dx = v*Math.cos(fi);
-		dy = v*Math.sin(fi);
+		v = v * Math.exp(-v / 1000);
+		dx = v * Math.cos(fi);
+		dy = v * Math.sin(fi);
 		if (Math.abs(dx) < 1 && Math.abs(dy) < 1) {
 			dx = 0;
 			dy = 0;
 		}
-		x += dx;   
+		x += dx;
 		y += dy;
-		
+
 		reflect();
+
+		assert getX() < Board.RIGHTBOARD && getX() > Board.LEFTBOARD;
+		assert getY() < Board.BOTTOMBOARD && getY() > Board.TOPBOARD;
 		
-		//TODO Check postcondition
 	}
 
 	private void reflect() {
-		if (Math.abs(x + IMG_TAM_X - Board.RIGHTBOARD) <  Math.abs(dx)) {
+		if (Math.abs(x + IMG_TAM_X - Board.RIGHTBOARD) < Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
-		if (Math.abs(y + IMG_TAM_Y - Board.BOTTOMBOARD) <  Math.abs(dy)) {
-			fi = - fi;
+		if (Math.abs(y + IMG_TAM_Y - Board.BOTTOMBOARD) < Math.abs(dy)) {
+			fi = -fi;
 		}
-		if (Math.abs(x - Board.LEFTBOARD) <  Math.abs(dx)) {
+		if (Math.abs(x - Board.LEFTBOARD) < Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
-		if (Math.abs(y - Board.TOPBOARD) <  Math.abs(dy)) {
-			fi = - fi;
+		if (Math.abs(y - Board.TOPBOARD) < Math.abs(dy)) {
+			fi = -fi;
 		}
 	}
 
 	public int getX() {
-		return (int)x;
+		return (int) x;
 	}
-	
+
 	public int getY() {
-		return (int)y;
+		return (int) y;
 	}
-	
+
 	public double getFi() {
 		return fi;
 	}
 
 	public double getdr() {
-		return Math.sqrt(dx*dx+dy*dy);
+		return Math.sqrt(dx * dx + dy * dy);
 	}
 
 	public void setX(double x) {
